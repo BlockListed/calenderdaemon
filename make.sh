16183c6cfd9b89f179ecfff068db30978278a438
BlockListed <github@aaronlowitzsch.com>
2021-02-18 23:38:57 +0100

    Modified make script

--- make.sh
+++ make.sh
@@ -3,11 +3,13 @@ then
     nim c caldaemon.nim
     cp caldaemon ~/.local/bin/
     cp helper ~/.local/bin/
-fi
 
-if [ $1 = uninstall ]
+elif [ $1 = uninstall ]
 then
     rm ~/.local/bin/caldaemon
     rm ~/.local/bin/helper
     echo "Remove ~/.config/cald to delete all files from this program!"
+
+else
+    echo "Please use either install or uninstall as the argument!"
 fi
